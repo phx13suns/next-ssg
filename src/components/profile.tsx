d6ebc35029767583118802150172a022b3254b80
KING <mambacurial8@gmail.com>
2024-02-03 08:43:48 +0900

    refactor

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import iconSrc from '../../images/icon.png'
+import iconSrc from '../images/icon.png'
 
 export const Profile: React.FC = () => {
   return (
