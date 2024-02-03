f8fd007a1df4fbfe17b793e6cbb21619c397c136
KING <mambacurial8@gmail.com>
2024-02-04 06:58:24 +0900

    refactor

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 import { twJoin } from 'tailwind-merge'
 
@@ -15,16 +16,7 @@ export const Breadcrumbs: React.FC<Props> = ({ items, className }) => {
       <ol>
         <li>
           <Link href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              role="graphics-symbol"
-              className="h-4 w-4"
-            >
-              <title>Home</title>
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
+            <HomeIcon title="home" role="graphics-symbol" aria-hidden="false" className="w-4 h-4" />
           </Link>
         </li>
         {items.map(item => (
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,5 +1,6 @@
 import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
+import { twJoin } from 'tailwind-merge'
 
 export type Props = {
   path: string
@@ -25,8 +26,12 @@ export const Pagination: React.FC<Props> = ({ path, currentPage, articleCount, a
               href={`${path}/1`}
               className="flex items-center justify-center px-3 h-full bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
             >
-              <span className="sr-only">first page</span>
-              <ChevronDoubleLeftIcon className="h-4 w-4" />
+              <ChevronDoubleLeftIcon
+                title="first page"
+                role="graphics-symbol"
+                aria-hidden="false"
+                className="h-4 w-4"
+              />
             </Link>
           )}
         </li>
@@ -36,7 +41,10 @@ export const Pagination: React.FC<Props> = ({ path, currentPage, articleCount, a
             <li key={index}>
               <Link
                 href={`${path}/${index}`}
-                className="flex items-center justify-center px-3 h-full bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                className={twJoin(
+                  'flex items-center justify-center px-3 h-full border border-gray-300 hover:bg-gray-100 hover:text-gray-700',
+                  currentPage === index ? 'bg-blue-50' : 'bg-white'
+                )}
                 aria-current={currentPage === index ? 'page' : undefined}
               >
                 {index}
@@ -54,8 +62,12 @@ export const Pagination: React.FC<Props> = ({ path, currentPage, articleCount, a
               href={`${path}/${pageCount}`}
               className="flex items-center justify-center px-3 h-full bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
             >
-              <span className="sr-only">last page</span>
-              <ChevronDoubleRightIcon className="h-4 w-4" />
+              <ChevronDoubleRightIcon
+                title="last page"
+                role="graphics-symbol"
+                aria-hidden="false"
+                className="h-4 w-4"
+              />
             </Link>
           )}
         </li>
