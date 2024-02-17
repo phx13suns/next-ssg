c4bc07b38401fa3e64535d9902d42cfa201f1d8b
KING <mambacurial8@gmail.com>
2024-02-17 20:26:45 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>title</h1>
+      <h1 className="text-3xl">phx13suns Blog</h1>
+      <Link href="/blog/">Enter</Link>
       <Link href="/about/">About</Link>
     </main>
   )
